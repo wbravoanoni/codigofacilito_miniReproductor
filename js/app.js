e348ebec8dae595d23096b4ad4462f37ad1f3a98
wbravoanoni <wbravoanoni@gmail.com>
2018-01-18 12:12:21 -0300

    Listado de caciones

--- js/app.js
+++ js/app.js
@@ -12,7 +12,20 @@ var music;
 function getSongs(){
 $.getJSON("js/app.json",function(mjson){
 music=mjson;
+genList(music);
 console.log(music);
 
 });
+}
+
+function genList(music){
+$.each(music.songs,function(i,song){
+$('#playlist').append('<li class="list-group-item" id="'+i+'">'+song.nombre+'</li>');
+});
+
+$("#playlist li").click(function(){
+var selectedSoung=$(this).attr('id');
+console.log(selectedSoung);
+});
+
 }
